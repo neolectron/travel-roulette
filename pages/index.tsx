f0d726c5a1f72a9a1d504cb32cd8817e07544e40
neolectron <xeolectron@gmail.com>
2021-09-25 22:25:01 +0200

    feat(IndexPage): IndexPage first draft, basic design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,25 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
 
-const Home: NextPage = () => {
-  return <div>aya</div>;
+import Button from '../components/Button/Button';
+
+interface HomePageProps {}
+
+const HomePage: NextPage<HomePageProps> = () => {
+  return (
+    <main className="bg-image-primary flex flex-col min-h-screen bg-cover">
+      <h1 className="text-quartenary mt-32 text-4xl font-bold text-center">
+        Have you ever been to Africa ?
+      </h1>
+      <div className="flex justify-center mt-12">
+        <Link href="/discover">
+          <a>
+            <Button text="Discover the world !" />
+          </a>
+        </Link>
+      </div>
+    </main>
+  );
 };
 
-export default Home;
+export default HomePage;
